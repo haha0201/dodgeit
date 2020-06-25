ceaa4c39e7e718d12c61ca3eadf7725bc48c0946
ZeroTixDev <67355595+ZeroTixDev@users.noreply.github.com>
2020-06-25 14:37:31 -0400

    Update main.js

--- main.js
+++ main.js
@@ -41,6 +41,8 @@ function mouseClicked(){
 
 }
 function loadImages(){
+  imgLogo = createImg('https://cdn.discordapp.com/attachments/720377166424178728/725772850580947074/ddodge_logo_final.png','');
+  imgLogo.hide();
   imgJotunn = createImg('https://evades.io/winter-wreath.07f00139.png', "");
 imgJotunn.hide();
   imgJotunnPower1 = createImg('https://evades.io/decay.b84a5b9a.png',"");
@@ -67,7 +69,9 @@ imgJotunn.hide();
 function setup() {
   frameRate(60);
   engine.start();
+  if(mainMenu){
   win = new Vec(windowWidth,windowHeight);
+  }
   herocolors = [ new Color(245, 12, 12), new Color(19, 165, 191), new Color(89, 76, 112)]
   createCanvas(win.x, win.y);  
   loadImages();
@@ -86,75 +90,7 @@ function windowResized() {
   win = new Vec(windowWidth, windowHeight)
   resizeCanvas(win.x, win.y);
 }
-function Menu(){
-  background(herocolors[currentHero].r,herocolors[currentHero].g,herocolors[currentHero].b)
-textFont("Maven Pro");
-  stroke(0);
-  strokeWeight(2)
- rect(win.x/2-200,win.y/2+200,400,50);
-  textSize(90);
-  text("Dodge It",win.x/2-180,150);
-  textSize(20);
-  strokeWeight(1);
-  text("I'm not responsible for any damage that happens to your computer. This is a rage game.",win.x/2-320,200);
-  textSize(30);
-  textFont("Georgia");
-  text("PLAY",win.x/2-50,win.y/2+235);
-  rect(50,305,200,50);
-  text(`Hero:${heros[currentHero]}`,50,295);
-  textFont("Helvetica");
-  text(`Switch Hero`,60,340);
-  rect(win.x-250,305,200,50);
-  text(`Switch Mode`,win.x-240,340);
-  text(`Mode:${modes[mode]}`,win.x-245,300);
-  textSize(25);
-  text(`WASD/Arrows keys to move:P to toggle outline:J and K or Z and X to use abilites:Good luck`,win.x/2-500,win.y-20);
 
-//  fill(255);
-  textSize(20)
-  strokeWeight(1.5);
-
-  textSize(20);
-  stroke(0);
-  strokeWeight(1.5);
-  if(heros[currentHero] !== 0){
-    if(heros[currentHero] == "Magmax"){
-    text(`Magmax's Abilites\nJ or Z to flow(makes you go 2x faster)\nK or X to harden(makes you invincible but you can't move and you \ncan harden up to 4 seconds with a cooldown of 3 seconds)`,win.x/2-300,win.y/2+100);
-}else if(heros[currentHero] == "Jotunn"){
-  text(`Jotunn's Abilites\nPassive Ability: If enemies are near you, they get 60% slower(Some enemies\naren't affected)\nShard Ability: K or Z to shatter enemies away(Some enemies aren't affected)`,win.x/2-300,win.y/2+100);
-}else if(heros[currentHero] == "Kopo"){
-  text(`Kopo's Abilites\nSmol: J or Z to make itself smaller\nDome Hole!(DH) : K or X to place down an aura(which traps enemies for a certain\namount of time and cannot kill you in the aura) when your smol`,win.x/2-300,win.y/2+100);
-}
-  }
-  if(mouseX>50 && mouseX < 250 && mouseY>305 && mouseY<355 && !switchHeroLock){
-    if(mouseP){
-    switchHeroLock = true;
-    if(heros.length-1 == currentHero){
-     currentHero = 0; 
-    }else{
-    currentHero++;
-    }
-    }
-  }else if(!mouseP){
-   switchHeroLock = false; 
-  }
-  if(mouseX>win.x/2-200 && mouseX < win.x/2+200 && mouseY>win.y/2 +200&& mouseY<win.y/2+250 && mouseP){
-   mainMenu = false; 
-    game.player.heroProperties(heros[currentHero]);
-    game.loadLevel();
-  }
-  if(mouseX<win.x-50 && mouseX>win.x-250 && mouseY>305 && mouseY<355&& !switchModeLock&& mouseP){
-    switchModeLock = true;
-   if(mode == modes.length-1){
-    mode = 0; 
-   }else{
-    mode++; 
-   }
-  }
-  if(!mouseP){
-   switchModeLock = false; 
-  }
-}
 function update(dt){
   if(!mainMenu){
   if(game.player.pos.x+game.player.radius>360 && game.player.pos.y-game.player.radius<2760){
@@ -226,7 +162,7 @@ function update(dt){
 }
 function render(){  
   if(mainMenu&&time===0){
-    Menu();
+   display.menu();
   }else if(!finishedSpeedrun){
   display.draw(game,fov,playerCamera,win,outline,herocolors[currentHero]);
   if(game.player.hero =="Jotunn"){
