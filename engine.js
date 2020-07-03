6b1be384944c747a784c192500df64b0f878eb8d
ZeroTixDev <67355595+ZeroTixDev@users.noreply.github.com>
2020-07-02 22:29:40 -0400

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,30 +1,19 @@
   class Engine{
-   constructor(dt,update,render){
+   constructor(update,render){
      this.afr = undefined;
-     this.step = dt;
-     this.acc = 0;
      this.lastTime = window.performance.now();
      this.updated = false;
    }
-    run(time){
-     let delta = time-this.lastTime
-      this.lastTime = time;
-      this.acc+=delta;
-          if(this.acc>this.step*3){
-       this.acc=this.step; 
-      }
-      while(this.acc>=this.step){
-      update(this.step);
-        this.acc-=this.step;
-        this.updated = true;
-      }
-      if(this.updated){
-        this.updated = false;
-      render(this.step);
-      }
-      this.afr = window.requestAnimationFrame((time)=>{this.run(time)});
+    run(){
+      let now = ( (new Date().getTime()))
+     let delta =Math.min(now-this.lastTime,100)/1000
+      this.lastTime = now;
+      update(delta);
+      render(delta);
+      frames+=60*delta
+      this.afr = window.requestAnimationFrame(()=>{this.run()});
     }
     start(){
-     this.afr = window.requestAnimationFrame((time)=>{this.run(time)});
+     this.afr = window.requestAnimationFrame(()=>{this.run()});
     }
   }
