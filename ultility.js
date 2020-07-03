36f4b8adfd4c017db136583487998a10481ddf0e
ZeroTixDev <67355595+ZeroTixDev@users.noreply.github.com>
2020-07-02 22:28:14 -0400

    Update ultility.js

--- ultility.js
+++ ultility.js
@@ -1,4 +1,5 @@
 var frames = 0;
+const playSpeed = 60;
 class Vec{
  constructor(x,y){
  this.x = x;
@@ -20,8 +21,8 @@ class Fade{
    this.realtime = 0;
  }
   simulate(dt){
-    this.pos.x+=this.vel.x*(1000/60/16);
-    this.pos.y+=this.vel.y*(1000/60/16);
+    this.pos.x+=this.vel.x*(1);
+    this.pos.y+=this.vel.y*(1);
     if(this.type=="Magmax"){
       fill(99, 6, 6, this.time);
     }else if(this.type=="Jotunn"){
@@ -31,9 +32,9 @@ class Fade{
     }
     rect(this.pos.x,this.pos.y,this.size.x,this.size.y);
     if (this.realtime > 80){
-   this.time-=2.5*(1000/60/16)
+   this.time-=2.5*(1)
     }
-    this.realtime ++;
+    this.realtime+=1.5;
   }
 }
 class MenuButton{
@@ -45,10 +46,13 @@ class MenuButton{
    this.size = new Vec(50,50);
    this.type = type;
    this.time = 3;
+   this.touched = true;
+   this.hero =0;
  }
   simulate(dt){
+    this.touched = false;
   if(mouseX>this.pos.x-this.size.x&&mouseX<this.pos.x+this.size.x&&mouseY>this.pos.y-this.size.y&&mouseY<this.pos.y+this.size.y){
-    this.sa+=0.2*(dt/16)
+    this.sa+=50*(dt)
     if(this.type=="Magmax"&&this.vel.y===0){
      fill(0);
       noStroke();
@@ -56,6 +60,9 @@ class MenuButton{
       text("Magmax",win.x/2,win.y/2+win.y/4-25);
       textSize(20);
           text(`Magmax's Abilites\nJ or Z to flow(makes you go 2x faster)\nK or X to harden(makes you invincible but you can't move and you \ncan harden up to 4 seconds with a cooldown of 3 seconds)`,win.x/2,win.y/2+win.y/10);
+      image(imgMagmaxWow,win.x/2+win.x/3,win.y/2,150,150)
+      this.touched = true;
+       this.hero =0;
       textAlign(LEFT, BOTTOM);
     }else if(this.type=="Jotunn"&&this.vel.y===0){
         fill(0);
@@ -64,6 +71,9 @@ class MenuButton{
       text("Jotunn",win.x/2,win.y/2+win.y/4-25);
        textSize(20);
        text(`Jotunn's Abilites\nPassive Ability: If enemies are near you, they get 60% slower(Some enemies aren't affected)\nShard Ability: K or Z to shatter enemies away(Some enemies aren't affected)`,win.x/2,win.y/2+win.y/10);
+       image(imgJotunnThonk,win.x/2+win.x/3,win.y/2,150,150)
+      this.touched = true;
+       this.hero =1;
       textAlign(LEFT, BOTTOM);
     }else if(this.type=="Kopo"&&this.vel.y===0){
         fill(0);
@@ -71,15 +81,18 @@ class MenuButton{
       textAlign(CENTER, CENTER);
       text("Kopo",win.x/2,win.y/2+win.y/4-25);
        textSize(20);
-      text(`Kopo's Abilites\nSmol: J or Z to make itself smaller\nDome Hole!(DH) : K or X to place down an aura(which traps enemies for a certain\namount of time and cannot kill you in the aura) when your smol`,win.x/2,win.y/2+win.y/10);
+      text(`Kopo's Abilites\nSmol: J or Z to make itself smaller at the cost of a annoying trail\nDome Hole!(DH) : K or X to place down an aura(which traps enemies for a certain\namount of time and cannot kill you in the aura) when your smol`,win.x/2,win.y/2+win.y/10);
+       image(imgKopoGlasses,win.x/2+win.x/3,win.y/2,150,150)
+      this.touched = true;
+       this.hero =2;
       textAlign(LEFT, BOTTOM);
     }
   }
-    this.sa*=Math.pow(0.911,dt/16);
-    if(this.sa<1)this.sa =1;
-    if(this.sa>=1.7)this.sa = 1.7
+    this.sa*=Math.pow(0.911,1);
+    if(this.sa<1){this.sa =1;}
+    if(this.sa>=1.5){this.sa = 1.5}
     this.pos.y+=this.vel.y;
-    this.yv*=Math.pow(0.7,dt/16);
+    this.yv*=Math.pow(0.7,1);
    if(this.pos.y<this.stop.y){
     this.vel.y=0; 
    }
