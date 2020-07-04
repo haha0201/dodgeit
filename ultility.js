b711b681885983008c92f10aa2d486a4acaabcf1
ZeroTixDev <67355595+ZeroTixDev@users.noreply.github.com>
2020-07-04 13:29:32 -0400

    Update ultility.js

--- ultility.js
+++ ultility.js
@@ -1,5 +1,58 @@
 var frames = 0;
-const playSpeed = 120;
+const playSpeed = 60;
+var endtime = 0;
+var fireworks = [];
+class Firework{
+ constructor(x,y,dir,color,size){
+  this.x = x;
+   this.y = y;
+   this.dir = dir;
+   this.color = color;
+   this.xv = 7*cos(this.dir*Math.PI/4)
+   this.yv = 7*sin(this.dir*Math.PI/4);
+   this.fill = this.color;
+   this.gravity = -0.5;
+   this.size = size;
+ }
+  simulate(dt){
+   if(this.y>win.y+100){
+    this.delete = 0; 
+   }
+    this.x+=this.xv*playSpeed*dt
+    this.y+=this.yv*playSpeed*dt
+    this.gravity+=0.03*dt*playSpeed
+    this.yv+=this.gravity*dt*playSpeed;
+    ctx.fillStyle=`rgba(${this.fill.r},${this.fill.g},${this.fill.b},${this.fill.a})`
+    ctx.fillRect(this.x,this.y,this.size,this.size);
+  }
+}
+
+CanvasRenderingContext2D.prototype.wrapText = function (text, x, y, maxWidth, lineHeight) {
+
+    var lines = text.split("\n");
+
+    for (var i = 0; i < lines.length; i++) {
+
+        var words = lines[i].split(' ');
+        var line = '';
+
+        for (var n = 0; n < words.length; n++) {
+            var testLine = line + words[n] + ' ';
+            var metrics = this.measureText(testLine);
+            var testWidth = metrics.width;
+            if (testWidth > maxWidth && n > 0) {
+                this.fillText(line, x, y);
+                line = words[n] + ' ';
+                y += lineHeight;
+            } else {
+                line = testLine;
+            }
+        }
+
+        this.fillText(line, x, y);
+        y += lineHeight;
+    }
+};
 class Vec{
  constructor(x,y){
  this.x = x;
@@ -17,24 +70,26 @@ class Fade{
    this.size = new Vec(sizex,sizey);
    this.type=type;
    this.counted = false;
-   this.time = 255;
-   this.realtime = 0;
+   this.time = 1;
+   this.canGoDown =false;
  }
   simulate(dt){
-    this.pos.x+=this.vel.x*(1);
-    this.pos.y+=this.vel.y*(1);
+    this.pos.x+=this.vel.x*(dt)*playSpeed
+    this.pos.y+=this.vel.y*(dt)*playSpeed
     if(this.type=="Magmax"){
-      fill(99, 6, 6, this.time);
+      ctx.fillStyle= `rgba(99, 6, 6, ${this.time})`
     }else if(this.type=="Jotunn"){
-     fill(5, 78, 105, this.time); 
+     ctx.fillStyle=`rgba(5, 78, 105, ${this.time})`
     }else if(this.type=="Kopo"){
-     fill(23, 1, 54, this.time); 
+     ctx.fillStyle =`rgba(23, 1, 54, ${this.time})` 
     }
-    rect(this.pos.x,this.pos.y,this.size.x,this.size.y);
-    if (this.realtime > 80){
-   this.time-=2.5*(1)
+    ctx.fillRect(this.pos.x,this.pos.y,this.size.x,this.size.y);
+    if(this.pos.x<0&&!this.canGoDown){
+     this.canGoDown = true 
     }
-    this.realtime+=1.5;
+    if(this.canGoDown){
+     this.time-=0.15*dt 
+    }
   }
 }
 class MenuButton{
@@ -48,70 +103,80 @@ class MenuButton{
    this.time = 3;
    this.touched = true;
    this.hero =0;
+   this.stopped = false;
  }
   simulate(dt){
     this.touched = false;
-  if(mouseX>this.pos.x-this.size.x&&mouseX<this.pos.x+this.size.x&&mouseY>this.pos.y-this.size.y&&mouseY<this.pos.y+this.size.y){
+    ctx.font ="20px Trebuchet MS"
+   if(mouse.x>this.pos.x-(this.size.x*this.sa)/2&&mouse.x<this.pos.x+(this.size.x*this.sa)/2&&mouse.y>this.pos.y-(this.size.y*this.sa)/2&&mouse.y<this.pos.y+(this.size.y*this.sa)/2){
     this.sa+=50*(dt)
     if(this.type=="Magmax"&&this.vel.y===0){
-     fill(0);
-      noStroke();
-      textAlign(CENTER, CENTER);
-      text("Magmax",win.x/2,win.y/2+win.y/4-25);
-      textSize(20);
-          text(`Magmax's Abilites\nJ or Z to flow(makes you go 2x faster)\nK or X to harden(makes you invincible but you can't move and you \ncan harden up to 4 seconds with a cooldown of 3 seconds)`,win.x/2,win.y/2+win.y/10);
-      image(imgMagmaxWow,win.x/2+win.x/3,win.y/2,150,150)
+     ctx.fillStyle="rgb(0,0,0)"
+    //  noStroke();
+      ctx.textAlign ="center"
+      ctx.font = "30px Trebuchet MS"
+      ctx.fillText("Magmax",win.x/2,win.y/2+win.y/4-25);
+      
+    // ctx.font = ctx.font.replace(/\d+px/, "20px");
+      ctx.font ="17px Trebuchet MS"
+          ctx.wrapText(`Magmax's Abilites\nJ or Z to flow(makes you go 2x faster)\nK or X to harden(makes you invincible but you can't move and you\ncan harden up to 4 seconds with a cooldown of 3 seconds)`,win.x/2,win.y/2+win.y/12,win.x/2,20);
+      ctx.drawImage(imgMagmaxWow,win.x/2+win.x/3,win.y/2,150,150)
       this.touched = true;
        this.hero =0;
-      textAlign(LEFT, BOTTOM);
+      ctx.textAlign ="left"
     }else if(this.type=="Jotunn"&&this.vel.y===0){
-        fill(0);
-      noStroke();
-      textAlign(CENTER, CENTER);
-      text("Jotunn",win.x/2,win.y/2+win.y/4-25);
-       textSize(20);
-       text(`Jotunn's Abilites\nPassive Ability: If enemies are near you, they get 60% slower(Some enemies aren't affected)\nShard Ability: K or Z to shatter enemies away(Some enemies aren't affected)`,win.x/2,win.y/2+win.y/10);
-       image(imgJotunnThonk,win.x/2+win.x/3,win.y/2,150,150)
+        ctx.fillStyle ="black"
+   //   noStroke();
+      ctx.textAlign = "center"
+       ctx.font = "30px Trebuchet MS"
+      ctx.fillText("Jotunn",win.x/2,win.y/2+win.y/4-25);
+      ctx.font ="17px Trebuchet MS"
+       ctx.wrapText(`Jotunn's Abilites\nPassive Ability: If enemies are near you, they get 60% slower(Some enemies aren't affected)\nShard Ability: K or Z to shatter enemies away(Some enemies aren't affected)`,win.x/2,win.y/2+win.y/14,win.x/2,20);
+       ctx.drawImage(imgJotunnThonk,win.x/2+win.x/3,win.y/2,150,150)
       this.touched = true;
        this.hero =1;
-      textAlign(LEFT, BOTTOM);
+      ctx.textAlign = "left"
     }else if(this.type=="Kopo"&&this.vel.y===0){
-        fill(0);
-      noStroke();
-      textAlign(CENTER, CENTER);
-      text("Kopo",win.x/2,win.y/2+win.y/4-25);
-       textSize(20);
-      text(`Kopo's Abilites\nSmol: J or Z to make itself smaller at the cost of a annoying trail\nDome Hole!(DH) : K or X to place down an aura(which traps enemies for a certain\namount of time and cannot kill you in the aura) when your smol`,win.x/2,win.y/2+win.y/10);
-       image(imgKopoGlasses,win.x/2+win.x/3,win.y/2,150,150)
+     ctx.fillStyle="black"
+     // noStroke();
+      //textAlign(CENTER, CENTER);
+      ctx.textAlign = "center"
+        ctx.font = "30px Trebuchet MS"
+      ctx.fillText("Kopo",win.x/2,win.y/2+win.y/4-25);
+   ctx.font ="17px Trebuchet MS"
+      ctx.wrapText(`Kopo's Abilites\nSmol: J or Z to make itself smaller at the cost of a annoying trail\nDome Hole!(DH) : K or X to place down an aura(which traps enemies for a certain\namount of time and cannot kill you in the aura) when your smol`,win.x/2,win.y/2+win.y/14,win.x/2,20);
+       ctx.drawImage(imgKopoGlasses,win.x/2+win.x/3,win.y/2,150,150)
       this.touched = true;
        this.hero =2;
-      textAlign(LEFT, BOTTOM);
+      ctx.textAlign = "left"
     }
   }
-    this.sa*=Math.pow(0.911,1);
-    if(this.sa<1){this.sa =1;}
-    if(this.sa>=1.5){this.sa = 1.5}
+   this.sa*=0.99
+    if(this.sa<=1){this.sa =1;}
+    if(this.sa>=1.7){this.sa=1.7}
     this.pos.y+=this.vel.y;
     this.yv*=Math.pow(0.7,1);
    if(this.pos.y<this.stop.y){
     this.vel.y=0; 
+     this.stopped = true;
    }
+      ctx.lineWidth = 6*this.sa
     if(this.type=='Jotunn'){
-    fill(25, 213, 255, this.time);
+    ctx.fillStyle= `rgb(25, 213, 255, ${this.time})`
     }else if(this.type=='Magmax'){
-      fill(247, 42, 42, this.time);
+      ctx.fillStyle = `rgb(247, 42, 42,${this.time})`
     }else if(this.type=="Kopo"){
-     fill(149, 26, 219, this.time); 
+     ctx.fillStyle=`rgb(149, 26, 219,${this.time})`
     }
-     rect(this.pos.x-(this.size.x*this.sa)/2, this.pos.y-(this.size.y*this.sa)/2, this.size.x*this.sa, this.size.y*this.sa)
+     ctx.fillRect(this.pos.x-(this.size.x*this.sa)/2,+this.pos.y-(this.size.y*this.sa)/2, this.size.x*this.sa, this.size.y*this.sa)
     if(this.type=="Jotunn"){
- fill(14, 115, 138, this.time);
+ ctx.fillStyle =`rgb(14, 115, 138, ${this.time})`
     }else if(this.type=="Magmax"){
-     fill(84, 5, 5, this.time); 
+     ctx.fillStyle =`rgb(84, 5, 5, ${this.time})`
     }else if(this.type=="Kopo"){
-     fill(50, 8, 74, this.time); 
+     ctx.fillStyle = `rgb(50, 8, 74,${this.time})`
     }
-  rect(this.pos.x-(this.size.x*this.sa)/2, this.pos.y+(this.size.y*this.sa)/69420, this.size.x*this.sa, this.size.y*this.sa/2)
+  ctx.fillRect(this.pos.x-(this.size.x*this.sa)/2, this.pos.y+(this.size.y*this.sa)/69420, this.size.x*this.sa, this.size.y*this.sa/2)
   if (this.time < 500){
   this.time*= 1.2;
   }
@@ -133,10 +198,13 @@ class Safe{
  }
 }
 class Color{
- constructor(r,g,b){
+ constructor(r,g,b,a){
   this.r = r;
    this.g = g;
    this.b = b;
+   if(a){
+    this.a = a; 
+   }
  }
 }
 class Teleporter{
